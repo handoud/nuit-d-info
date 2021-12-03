5dce80ae00a34ec067e282c2e58ea8c80bcf86a2
handoud <79791818+handoud@users.noreply.github.com>
2021-12-03 03:44:30 +0000

    Add files via upload

---
+++ nuitinfo/main.js
@@ -0,0 +1,31 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+
+
+function settingsMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
+
+}
+
+darkBtn.onclick = function() {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("darck-theme");
+
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+
+}
+
+if (localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("darck-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("darck-theme");
+} else {
+    localStorage.setItem("theme", "light");
+}
